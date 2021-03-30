61eb26f99b52e51b5e9b88dec942b8bc7f3ba89f
ShellGratuit <ademderbal88@gmail.com>
2021-03-30 14:11:32 +0200

    test

--- api/index.php
+++ api/index.php
@@ -4,6 +4,7 @@ use \Psr\Http\Message\ResponseInterface as Response;
 
 require '../vendor/autoload.php';
 
+
 $app = new \Slim\App;
 $app->get('/hello/{name}', function (Request $request, Response $response, array $args) {
     $name = $args['name'];
