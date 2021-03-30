926e38d493f1bb9eed128647db5d19e7167175d3
ShellGratuit <ademderbal88@gmail.com>
2021-03-30 12:41:05 +0200

    test

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Factory\AppFactory;
+require __DIR__ . '/vendor/autoload.php';
+$app = AppFactory::create();
+$app->get('/hello/{name}',
+    function (Request $resquest, Response $response,$args) {
+        $array = array('nom' => $args['name']);
+
+        return $response->getBody()->write(json_encode($array));
+
+
+    });
+
+$app->run();
+?>
