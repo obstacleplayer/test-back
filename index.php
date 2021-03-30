71c9b5ed6bd3d100487cc57aab4c142ea883ce77
ShellGratuit <ademderbal88@gmail.com>
2021-03-30 13:33:26 +0200

    add autoload

--- index.php
+++ index.php
@@ -1,9 +1,11 @@
 <?php
+require 'vendor/autoload.php';
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
 require __DIR__ . '/vendor/autoload.php';
 $app = AppFactory::create();
+
 $app->get('/hello/{name}',
     function (Request $resquest, Response $response,$args) {
         $array = array('nom' => $args['name']);
