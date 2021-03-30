a350eaea8fd67d5545a7eb4e4d62e55716431984
ShellGratuit <ademderbal88@gmail.com>
2021-03-30 13:44:12 +0200

    move index.php to directory api

--- api/index.php
+++ api/index.php
@@ -3,7 +3,7 @@ require 'vendor/autoload.php';
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
-require __DIR__ . '/vendor/autoload.php';
+require __DIR__ . '../vendor/autoload.php';
 $app = AppFactory::create();
 
 $app->get('/hello/{name}',
